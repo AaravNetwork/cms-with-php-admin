5cc0def72932f63d44c5583afb88859ade731a08
Aarav <tataimishra123@gmail.com>
2023-09-02 00:43:10 +0530

    Final message of 1st Sep 2023

--- admin/posts.php
+++ admin/posts.php
@@ -13,50 +13,33 @@
                         Welcome to Admin
                         <small>Tatai</small>
                     </h1>
-                    <table class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Author</th>
-                                <th>Title</th>
-                                <th>Category Id</th>
-                                <th>Status</th>
-                                <th>Image</th>
-                                <th>Tags</th>
-                                <th>Comments Count</th>
-                                <th>Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php 
-                                $query = "SELECT * FROM posts";
-                                $select_all_queries = mysqli_query($conn, $query);
-                                while($row = mysqli_fetch_assoc($select_all_queries)) {
-                                    $post_id = $row['post_id'];
-                                    $post_author = $row['post_author'];
-                                    $post_title = $row['post_title'];
-                                    $post_category_id = $row['post_category_id'];
-                                    $post_status = $row['post_status'];
-                                    $post_image = $row['post_image'];
-                                    $post_tags = $row['post_tags'];
-                                    $post_comments_count = $row['post_comment_count'];
-                                    $post_date = $row['post_date'];
+                    <?php
 
-                                    echo "<tr>";
-                                    echo "<td style='vertical-align: middle;'>{$post_id}</td>";
-                                    echo "<td style='vertical-align: middle;'>{$post_author}</td>";
-                                    echo "<td style='vertical-align: middle;'>{$post_title}</td>";
-                                    echo "<td style='vertical-align: middle;'>{$post_category_id}</td>";
-                                    echo "<td style='vertical-align: middle;'>{$post_status}</td>";
-                                    echo "<td style='vertical-align: middle;'><img width='50' src='../images/{$post_image}'></td>";
-                                    echo "<td style='vertical-align: middle;'>{$post_tags}</td>";
-                                    echo "<td style='vertical-align: middle;'>{$post_comments_count}</td>";
-                                    echo "<td style='vertical-align: middle;'>{$post_date}</td>";
-                                    echo "</tr>";
-                                }                                
-                            ?>
-                        </tbody>
-                    </table>
+                        if(isset($_GET['source'])) {
+                            $source = $_GET['source'];
+                        } else {
+                            $source = '';
+                        }
+                        switch($source) {
+                            case '34';
+                            echo 'Nice 34';
+                            break;
+
+                            case '100';
+                            echo 'Nice 100';
+                            break;
+
+                            case '200';
+                            echo 'Nice 200';
+                            break;
+
+
+                            default:
+                            include "includes/view_all_posts.php";
+                            break;
+                        }
+
+                    ?>
                 </div>
             </div>
             <!-- /.row -->
